695eeb627232349201475b74f580fac80534e40e
Henrique <heduardocr7@gmail.com>
2024-08-27 20:54:57 -0300

    feat: first activity of seminar 04

--- index.js
+++ index.js
@@ -3,33 +3,72 @@ import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-  connectionStateRecovery: {
-    maxDisconnectionDuration: 180000,
-  },
-});
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-io.on("connection", (socket) => {
-  if (socket.recovered) {
-    console.log("eita, recuperei");
-  }
-
-  socket.on("send message", (msg) => {
-    io.emit("send message", msg);
+async function main() {
+  const db = await open({
+    filename: "chat.db",
+    driver: sqlite3.Database,
   });
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
+
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {
+      maxDisconnectionDuration: 180000,
+    },
   });
-});
 
-app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"));
-});
+  const __dirname = dirname(fileURLToPath(import.meta.url));
 
-server.listen(3000, () => console.log("running at port 3000"));
+  io.on("connection", async (socket) => {
+    if (socket.recovered) {
+      console.log("eita, recuperei");
+
+      const messages = await db.all(
+        "SELECT content FROM messages ORDER BY id ASC"
+      );
+      socket.emit(
+        "message history",
+        messages.map((msg) => msg.content)
+      );
+    }
+
+    io.emit("user connected", "Usuário conectado");
+
+    socket.on("send message", async (msg) => {
+      let result;
+      try {
+        result = await db.run("INSERT INTO messages (content) VALUES (?)", msg);
+      } catch (e) {
+        console.error("Erro ao salvar mensagem:", e);
+        return;
+      }
+
+      io.emit("send message", msg, result.lastID);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+
+      io.emit("user disconnected", "Um usuário desconectou-se");
+    });
+  });
+
+  app.get("/", (req, res) => {
+    res.sendFile(join(__dirname, "index.html"));
+  });
+
+  server.listen(3000, () => console.log("running at port 3000"));
+}
+
+main();
